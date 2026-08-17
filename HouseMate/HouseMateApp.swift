3c9b3107b5ab77dca33118ffaa369dcfae39f4bc
Mamricn <marcin.turek.27@wp.pl>
2026-08-17 19:29:34 +0100

    Initial Commit

---
+++ HouseMate/HouseMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseMateApp.swift
+//  HouseMate
+//
+//  Created by Marcin Turek on 17/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct HouseMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HouseMateTests/HouseMateTests.swift
@@ -0,0 +1,19 @@
+//
+//  HouseMateTests.swift
+//  HouseMateTests
+//
+//  Created by Marcin Turek on 17/08/2026.
+//
+
+import Testing
+@testable import HouseMate
+
+struct HouseMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
